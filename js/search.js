00c5af7ed8bbd482bb7d9b90f721c0bd263446ec
Paolo Galeone <nessuno@nerdz.eu>
2014-09-08 15:39:33 +0200

    Add support for search page

---
+++ js/search.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    var load = true;
+    var tmpDivId = "scrtxt";
+    var qs = $("#qs").html();
+    var plist = $("#postlist");
+
+    N.html.search.globalPosts(10, qs, function(d) {
+        plist.html(d);
+        window.fixHeights();
+    });
+
+    $(".showcomments").click(function(e) {
+        alert(1);
+        console.log('www');
+        $(this).trigger(e);
+    });
+
+    $(window).scroll(function() {
+        if($(this).scrollTop()+200 >= ( $(document).height() - $(this).height() ))
+        {
+            var num = 10;
+            var hpid = plist.find("div[id^='post']").last().data('hpid');
+            var append = '<h3 id="'+tmpDivId+'">'+N.getLangData().LOADING+'...</h3>';
+
+            if(load && !$("#"+tmpDivId).length)
+            {
+                plist.append(append);
+            }
+
+            if(load) {
+                load = false;
+                N.html.search.globalPostsBeforeHpid(num, $("#qs").html(), hpid, function(data) {
+                    $("#"+tmpDivId).remove();
+                    if(data.length > 0) {
+                        plist.append(data);
+                        window.fixHeights();
+                        load = true;
+                    }
+                });
+            }
+        }
+    });
+
+});
