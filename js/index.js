9f7e7ae7d2aaacd6bd85b4c8f12916988579a5f3
Paolo Galeone <nessuno@nerdz.eu>
2014-10-07 04:56:16 -0700

    Remove useless console.log

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
     var tz = jstz.determine();
-    console.log(tz.name());
     var select = $("#regfrm select[name=timezone]"), opt = select.find("option[value='"+tz.name()+"']");
-    console.log(select, opt);
     if(opt.length) {
         select.val(tz.name());
     }
