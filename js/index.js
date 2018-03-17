7b532e507d1dd08bcf14df0f7329b0d3a45e8164
Paolo Galeone <nessuno@nerdz.eu>
2018-03-17 11:05:29 +0100

    remove duplicate id in widgets. Fix timezone approximation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var tz = jstz.determine();
-    var select = $("#regfrm select[name=timezone]"), opt = select.find("option[value='" + tz.name() + "']");
+    var select = $("#reg-timezone"), opt = select.find("option[value='" + tz.name() + "']");
     if (opt.length) {
         select.val(tz.name());
     }
