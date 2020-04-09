98fc8bce403b8fbc2bd673e196eb2823ce17fef6
Paolo Galeone <nessuno@nerdz.eu>
2020-04-09 19:05:41 +0200

    registration message in #reg-error div

--- js/default.js
+++ js/default.js
@@ -185,12 +185,12 @@ $(document).ready(function() {
 		event.preventDefault();
 		N.json.register($("#regfrm").serialize(), function(obj) {
 			if (obj.status == "error") {
-				$("#error").html(obj.message.replace(/\n/g, "<br />"));
+				$("#reg-error").html(obj.message.replace(/\n/g, "<br />"));
 				$("#cptxt").html("");
 				N.reloadCaptcha();
 			} else {
 				if (obj.status == "ok") {
-					$("#error").hide();
+					$("#reg-error").hide();
 					$("#done").html(obj.message);
 					setTimeout(function() {
 						window.location.reload();
